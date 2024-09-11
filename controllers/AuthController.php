151bba97b39af8c4c19cc2269ec2d798414f4645
okayVeronica <veronicaborgesbarboza123@gmail.com>
2024-09-11 09:32:04 -0300

    colate a ação de URL, usando operador de coalasência e chamando o método de login

---
+++ controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+// Requer o arquivo user que contém o model user com as funções de manipulção de dados de usuários
+require_once 'models/user.php';
+
+class AuthController
+
+{
+    //função responsálvel processo de login
+    public function login()
+    {
+        // Verifica se a requisição HTTP é do POST, ou seja, se o formulário foi enviado 
+    }
+}
+
+?>
---
+++ routes.php
@@ -0,0 +1,21 @@
+<?php
+
+// Inclui arquevos de controlador nessesario para lidar com diferentes ações 
+require 'controllers/Authcontroller.php'; // Inclui o comtroladoe de autenticação 
+require 'controllers/UserController.php'; //Inclui o controlador de usuário 
+require 'controllers/DashboardController.php'; // Inclui o controlador de dashaboard 
+
+// Criar insâncias dos controladores para utilizar seus métodos 
+$authController = new AuthController(); //Instancia controlador de autenticação 
+$userControoler = new UserContreller(); // Instancia controlador de usuário
+$dashboardController = new DashboardController(); // Instancia controlador de dashboard
+
+// Coleta a ação de URL, se não houver ação definida, usa 'login' como padrão
+$action = $_GET['action'] ?? 'login'; // Usa operador de coalescência nula (??) para definir 'login' se ´action´ não estiver presente
+
+switch($action){
+    case 'login':
+        $authController ->login(); // chama o método de login do controlador de autenticação 
+}
+
+?>
